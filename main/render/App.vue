b9abaa4000ae3580d4ec3202023e143307b6d864
sifu <2587107273@qq.com>
2021-06-02 18:03:51 +0800

    主项目搭建基础layout框架

--- main/render/App.vue
+++ main/render/App.vue
@@ -1,35 +1,38 @@
 <template>
-    <div class="mainapp">
-        <!-- 标题栏 -->
-        <header class="mainapp-header">
-            <h1>Vue3-Micro</h1>
-        </header>
-        <div class="mainapp-main">
-            <!-- 侧边栏 -->
-            <ul class="mainapp-sidemenu">
-                <li @click="push('/app1')">app1</li>
-                <li @click="push('/app2')">app2</li>
-            </ul>
-            <!-- 子应用  -->
-            <div class="subapp-container">
-                <a-button>aa</a-button>
-                <a-card title="Default size card" style="width: 300px">
-                    <template #extra><a href="#">more</a></template>
-                    <p>card content</p>
-                    <p>card content</p>
-                    <p>card content</p>
-                </a-card>
-                <h4 v-if="loading" class="subapp-loading">Loading...</h4>
-                <div id="main-viewport"></div>
-            </div>
-        </div>
-    </div>
+<!--    <div class="mainapp">-->
+<!--        &lt;!&ndash; 标题栏 &ndash;&gt;-->
+<!--        <header class="mainapp-header">-->
+<!--            <h1>Vue3-Micro</h1>-->
+<!--        </header>-->
+<!--        <div class="mainapp-main">-->
+<!--            &lt;!&ndash; 侧边栏 &ndash;&gt;-->
+<!--            <ul class="mainapp-sidemenu">-->
+<!--                <li @click="push('/app1')">app1</li>-->
+<!--                <li @click="push('/app2')">app2</li>-->
+<!--            </ul>-->
+<!--            &lt;!&ndash; 子应用  &ndash;&gt;-->
+<!--            <div class="subapp-container">-->
+<!--                <a-button>aa</a-button>-->
+<!--                <a-card title="Default size card" style="width: 300px">-->
+<!--                    <template #extra><a href="#">more</a></template>-->
+<!--                    <p>card content</p>-->
+<!--                    <p>card content</p>-->
+<!--                    <p>card content</p>-->
+<!--                </a-card>-->
+<!--                <h4 v-if="loading" class="subapp-loading">Loading...</h4>-->
+<!--                <div id="main-viewport"></div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+    <main-layout></main-layout>
 </template>
 
 <script>
 import { toRef, ref, reactive } from 'vue'
+import MainLayout from "./components/main-layout.vue";
 export default {
     name: "App",
+    components: {MainLayout},
     setup(props){
         const loading = reactive(props.loading)
 
--- main/render/VueRender.js
+++ main/render/VueRender.js
@@ -1,11 +1,14 @@
 import {createApp} from 'vue';
 import App from './App.vue'
-import {Button,Card} from 'ant-design-vue';
+// import {Button,Card} from 'ant-design-vue';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
 function vueRender({ loading }) {
   return createApp(App,{loading})
-      .use(Button)
-      .use(Card)
+      .use(Antd)
+      // .use(Button)
+      // .use(Card)
       .mount('#app-container');
 }
 
